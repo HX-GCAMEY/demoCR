13adf90ff22803f0a3ee40f038dd48d473a7e44b
Gama <lic.camey@gmail.com>
2024-06-06 16:11:28 +0100

    FIX/ Drop schema

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -15,7 +15,6 @@ const config = {
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
   synchronize: true,
-  dropSchema: true,
 };
 
 export default registerAs('typeorm', () => config);
