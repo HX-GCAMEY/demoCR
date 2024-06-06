cdb859845f1fc02101bb8fb03c55ca214617b3d5
Gama <lic.camey@gmail.com>
2024-06-06 16:01:06 +0100

    ADD/ variable entorno HOST

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -6,7 +6,7 @@ dotenvConfig({ path: '.development.env' });
 
 const config = {
   type: 'postgres',
-  host: 'postgresdb',
+  host: `${process.env.DB_HOST}`,
   port: `${process.env.DB_PORT}`,
   username: `${process.env.DB_USER}`,
   password: `${process.env.DB_PASSWORD}`,
